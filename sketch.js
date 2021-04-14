df1816d24959d76556d3ee45deda81514b139d64
sanvi-kotha <80798319+sanvi-kotha@users.noreply.github.com>
2021-04-13 19:11:44 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ var jerry, jerryImg1,jerryImg2, jerryImg3;
 
 function preload() {
     //load the images here
-    bg=loadImage("images/garden.png");
-    tomImg1=loadAnimation("images/cat1.png");
-    tomImg2=loadAnimation("images/cat2.png","images/cat3.png");
-    tomImg3=loadAnimation("images/cat4.png");
-    jerryImg1=loadAnimation("images/mouse1.png");
-    jerryImg2=loadAnimation("images/mouse2.png", "images/mouse3.png");
-    jerryImg3=loadAnimation("images/mouse4.png");
+    bg=loadImage("garden.png");
+    tomImg1=loadAnimation("cat1.png");
+    tomImg2=loadAnimation("cat2.png","cat3.png");
+    tomImg3=loadAnimation("cat4.png");
+    jerryImg1=loadAnimation("mouse1.png");
+    jerryImg2=loadAnimation("mouse2.png", "mouse3.png");
+    jerryImg3=loadAnimation("mouse4.png");
 }
 
 function setup(){
